f978d07dd6f4f0bba55cab28eebd8d7fa5da7288
ammoore00 <abigailmmoore00@gmail.com>
2022-08-01 11:13:17 -0600

    Added missing ingredients to diet groups

--- overrides/kubejs/server_scripts/recipes/food.js
+++ overrides/kubejs/server_scripts/recipes/food.js
@@ -11,3 +11,10 @@ onEvent('recipes', event => {
         ]
     );
 });
+
+onEvent('tags.items', event => {
+    event.add('diet:fruits', ['farmersrespite:coffee_berries', 'ecologics:coconut_slice', 'ecologics:prickly_pear', 'ecologics:cooked_prickly_pear']);
+    event.add('diet:grains', ['corndelight:corn', 'corndelight:corn_seeds']);
+    event.add('diet:proteins', ['goodall:raw_venison', 'goodall:cooked_venison', 'biomemakeover:glowshrrom_stew', 'biomemakeover:raw_toad', 'biomemakeover:cooked_toad', 'ecologics:crab_meat', 'ecologics:walnut']);
+    event.add('diet:vegetables', ['biomemakemover:bulbus_root', 'biomemakeover:roasted_bulbus_root']);
+});
