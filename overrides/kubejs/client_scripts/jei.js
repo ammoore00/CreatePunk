c1138bd8f70628ad706a013eaaeeaff9dcbd6862
ammoore00 <abigailmmoore00@gmail.com>
2022-08-01 13:43:51 -0600

    Hid some items from jei, removed some quark overlap

--- overrides/kubejs/client_scripts/jei.js
+++ overrides/kubejs/client_scripts/jei.js
@@ -1,5 +1,23 @@
 onEvent('jei.hide.items', event => {
+    event.hide('minecraft:hopper');
+    event.hide('littlelogistics:rapid_hopper');
+
     event.hide('corn_delight:grilled_corn');
     event.hide('corn_delight:boiled_corn');
     event.hide('corn_delight:cornbread_stuffing');
+
+    event.hide('create:dough');
+
+    // Temporary, until beehive variants are properly added
+    event.hide('compatoplenty:cherry_beehive');
+    event.hide('compatoplenty:jacaranda_beehive');
+    event.hide('compatoplenty:fir_beehive');
+    event.hide('compatoplenty:redwood_beehive');
+    event.hide('compatoplenty:mahogany_beehive');
+    event.hide('compatoplenty:willow_beehive');
+    event.hide('compatoplenty:magic_beehive');
+    event.hide('compatoplenty:dead_beehive');
+    event.hide('compatoplenty:umbran_beehive');
+    event.hide('compatoplenty:palm_beehive');
+    event.hide('compatoplenty:hellbark_beehive');
 });
