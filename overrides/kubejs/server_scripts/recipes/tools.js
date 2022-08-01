4dd7f6d96841637f4b302900d5dfeb8c57895e4d
ammoore00 <abigailmmoore00@gmail.com>
2022-08-01 16:01:11 -0600

    Lots more documentation

--- overrides/kubejs/server_scripts/recipes/tools.js
+++ overrides/kubejs/server_scripts/recipes/tools.js
@@ -332,4 +332,8 @@ onEvent('tags.items', event => {
     event.add('forge:tools', 'kubejs:diamond_chisel');
     event.add('forge:tools/pickaxes', 'kubejs:diamond_chisel');
     event.add('forge:tools/diamond', 'kubejs:diamond_chisel');
-})
+
+    event.add('quark:boatable_chests', ['everycomp:q/twigs/stripped_bamboo_chest', 'everycomp:q/fruittrees/cherry_chest', 'everycomp:q/fruittrees/citrus_chest', 'everycomp:q/valhelsia_structures/lapidifed_jungle_chest']);
+    event.add('quark:revertable_chests', ['everycomp:q/twigs/stripped_bamboo_chest', 'everycomp:q/fruittrees/cherry_chest', 'everycomp:q/fruittrees/citrus_chest', 'everycomp:q/valhelsia_structures/lapidifed_jungle_chest']);
+    event.add('quark:revertable_trapped_chests', ['everycomp:q/twigs/stripped_bamboo_trapped_chest', 'everycomp:q/fruittrees/cherry_trapped_chest', 'everycomp:q/fruittrees/citrus_trapped_chest', 'everycomp:q/valhelsia_structures/lapidifed_jungle_trapped_chest']);
+});
